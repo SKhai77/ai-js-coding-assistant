ad5cc74c4b0e5dc5f2154c14a4fb98cd9df229a8
SKhai77 <orionkhai@gmail.com>
2024-08-25 12:35:36 -0500

    Create initial CLI structure with inquirer

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 require("dotenv").config();
+
+const inquirer = require("inquirer");
+
+const promptFunc = async () => {
+  while (true) {
+    const { userInput } = await inquirer.prompt([
+      {
+        type: "input",
+        name: "userInput",
+        message: "Type your JavaScript question (or type 'exit' to quit):",
+      },
+    ]);
+
+    if (userInput.toLowerCase() === "exit") {
+      console.log("\nExiting the application...\n");
+      break;
+    }
+
+    console.log(`You asked: ${userInput}`);
+  }
+};
+
+// Start the application
+promptFunc();
